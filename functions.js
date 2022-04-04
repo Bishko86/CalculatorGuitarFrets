67f8c085802813f233dc1434a936bb37f3c9b5cf
Bishko86 <bishko86@yahoo.com>
2022-04-04 15:49:15 +0300

    4.4.2022 | code refactor

--- functions.js
+++ functions.js
@@ -1,103 +1,118 @@
-"use strict"
-let unit = '';// cm, in
+"use strict";
+let unit = ""; // cm, in
 
 //choose unit measure - mm , inch, none
 
 function chooseUnit(event) {
-    let target = event.target;
-    if (target.id == 'none') unit = '';
-    if (target.id == 'mm') unit = 'mm';
-    if (target.id == 'in') unit = 'in';
+  let target = event.target;
+  if (target.id == "none") unit = "";
+  if (target.id == "mm") unit = "mm";
+  if (target.id == "in") unit = "in";
 }
 
 //shows warning if data incorect
 function invalidData(selector) {
-    let warning = document.querySelector(selector);
-    let message = '<i class="fa fa-exclamation-triangle fa-lg"></i>' + 'invalid data'
-    warning.innerHTML = message;
+  let warning = document.querySelector(selector);
+  let message =
+    '<i class="fa fa-exclamation-triangle fa-lg"></i>' + "invalid data";
+  warning.innerHTML = message;
 }
-
 
 // creates title of table and returns function that creates row table
 function addTable(scale, num, measure) {
-    const table = document.createElement('table');
-    const th = document.createElement('tr');
-    th.innerHTML = '<th> Fret #</th> <th>nut offset</th> <th>prev.fret</th>';
-    table.append(th);
-    table.setAttribute('id', 'table');
-    const titleInfo = document.querySelector('.calc-info');
-    titleInfo.innerHTML = `<pre id="titl-info">
+  const table = document.createElement("table");
+  const trHeader = document.createElement("tr");
+  trHeader.innerHTML = "<th> Fret #</th> <th>nut offset</th> <th>prev.fret</th>";
+  table.append(trHeader);
+  table.setAttribute("id", "table");
+  const titleInfo = document.querySelector(".calc-info");
+  titleInfo.innerHTML = `<pre id="titl-info">
 	Scale : ${scale} ${measure};
 	#fret : ${num}
 	</pre>`;
-    document.querySelector('#footer_block').before(table);
+  document.querySelector("#footer_block").before(table);
 
-    return function (...data) {
-        const tr = document.createElement('tr');
-        for (let i = 0; i < 3; i++) {
-            let td = document.createElement('td');
-            td.innerHTML = data[i];
-            tr.append(td);
-        }
-        table.append(tr);
+  return function (...data) {
+    const tr = document.createElement("tr");
+    tr.classList.add("table-row");
+    for (let i = 0; i < 3; i++) {
+      let td = document.createElement("td");
+      td.innerHTML = data[i];
+      tr.append(td);
     }
+    table.append(tr);
+  };
 }
-//calculator 
+
+//calculator
 function calcGuitarFrets(beaker, amount, constanta, measureUnit) {
-    const calc = addTable(beaker, amount, measureUnit);
-    let fromZero = 0;
-    let beetwenFret = 0;
-    for (let i = 0; i < amount; i++) {
-        let number = `<span id="number"> ${i + 1} </span>`;
-        beetwenFret = beaker / constanta;
-        beaker = beaker - beetwenFret;
-        fromZero += beetwenFret;
-        let dataFret = `<span class="data"> ${roundNumber(fromZero)} ${unit}</span>`;
-        let spaceBeetFret = `<span> ${roundNumber(beetwenFret)} ${unit}</span>`;
-        calc(number, dataFret, spaceBeetFret);
-    }
-    customEvent();
+  const calc = addTable(beaker, amount, measureUnit);
+  let fromZero = 0;
+  let beetwenFret = 0;
+  for (let i = 0; i < amount; i++) {
+    let number = `<span id="number"> ${i + 1} </span>`;
+    beetwenFret = beaker / constanta;
+    beaker = beaker - beetwenFret;
+    fromZero += beetwenFret;
+    let dataFret = `<span class="data"> ${roundNumber(
+      fromZero
+    )} ${unit}</span>`;
+    let spaceBeetFret = `<span> ${roundNumber(beetwenFret)} ${unit}</span>`;
+    calc(number, dataFret, spaceBeetFret);
+  }
+  customEvent();
 }
+
 // cuts amount decimal digits after point
 function roundNumber(number) {
-    let string = number.toString()
-    let index = string.indexOf(".")
-    let item = string.slice(0, index + 3);
-    return item;
+  let string = number.toString();
+  let index = string.indexOf(".");
+  let item = string.slice(0, index + 3);
+  return item;
 }
+
 //table ready
 function customEvent() {
-    let table = document.querySelector('#table');
-    setTimeout(function () {
-        table.dispatchEvent(new CustomEvent('ready-table', {
-            bubbles: true,
-            detail: {
-                children: table.children
-            }
-        }));
-    });
+  let table = document.querySelector("#table");
+  setTimeout(function () {
+    table.dispatchEvent(
+      new CustomEvent("ready-table", {
+        bubbles: true,
+        detail: {
+          children: table.children,
+        },
+      })
+    );
+  });
 }
+
 // set styles for download PDF file
 function styleTabPdf(display, position, color, pdn_data, fontSize) {
-    document.querySelector('#titl-info').style.fontSize = fontSize
-    document.querySelector('.back').style.display = display;
-    document.querySelector('.share').style.display = display;
-    let header = document.querySelector('.calcs-title');
-    header.style.cssText = `justify-content: ${position};
+  document.querySelector("#titl-info").style.fontSize = fontSize;
+  document.querySelector(".back").style.display = display;
+  document.querySelector(".share").style.display = display;
+  let header = document.querySelector(".calcs-title");
+  header.style.cssText = `justify-content: ${position};
 		background-color: ${color};
 		padding: ${pdn_data}
 		`;
 }
+
 // downloads pdf file of calculations
 async function generatePDF() {
-    styleTabPdf('none', 'center', '#fff', '0 0 0 0', '16px');
-    const element = document.querySelector('.calculation');
-    var opt = {
-        margin: 5,
-        filename: 'myfile.pdf',
-    }
-    await html2pdf().set(opt)
-        .from(element).save();
-    styleTabPdf('block', 'space-between', 'rgb(204, 203, 211);', '15px 0 15px 0', '20px');
+  styleTabPdf("none", "center", "#fff", "0 0 0 0", "16px");
+  const element = document.querySelector(".calculation");
+  var opt = {
+    margin: 5,
+    filename: "myfile.pdf",
+  };
 
+  await html2pdf().set(opt).from(element).save();
+  styleTabPdf(
+    "block",
+    "space-between",
+    "rgb(204, 203, 211);",
+    "15px 0 15px 0",
+    "20px"
+  );
 }
--- script.js
+++ script.js
@@ -1,5 +1,4 @@
-"use strict"
-// import { unit, chooseUnit, invalidData, calcGuitarFrets, styleTabPdf } from './functions.js'
+"use strict";
 
 //coeficient for calculate frets
 const coeficient = 17.817;
@@ -12,145 +11,144 @@ const beakerLength = form.elements.beaker;
 
 let length = beakerLength.value;
 
-beakerLength.addEventListener('input', inputLength);
+beakerLength.addEventListener("input", inputLength);
 
 function inputLength() {
-    const btnCalc = document.querySelector('#calc');
-    if (isNaN(beakerLength.value)) {
-        invalidData('#invalidCase');
-        btnCalc.setAttribute('disabled', 'true');
-    }
-    else {
-        document.querySelector('#invalidCase').innerHTML = '';
-        length = beakerLength.value;
-        btnCalc.removeAttribute('disabled');
-    }
+  const btnCalc = document.querySelector("#calc");
+  if (isNaN(beakerLength.value)) {
+    invalidData("#invalidCase");
+    btnCalc.setAttribute("disabled", "true");
+  } else {
+    document.querySelector("#invalidCase").innerHTML = "";
+    length = beakerLength.value;
+    btnCalc.removeAttribute("disabled");
+  }
 }
-//data for amount of frets 
+//data for amount of frets
 const frets = form.elements.fret;
 
 let amountFrets = frets.value;
 
-frets.addEventListener('input', inputFret);
+frets.addEventListener("input", inputFret);
 
 function inputFret() {
-    const btnCalc = document.querySelector('#calc');
-    if (isNaN(frets.value)) {
-        invalidData('#invalCase');
-        btnCalc.setAttribute('disabled', 'true');
-    }
-    else {
-        document.querySelector('#invalCase').innerHTML = '';
-        amountFrets = frets.value;
-        btnCalc.removeAttribute('disabled');
-    }
+  const btnCalc = document.querySelector("#calc");
+  if (isNaN(frets.value)) {
+    invalidData("#invalCase");
+    btnCalc.setAttribute("disabled", "true");
+  } else {
+    document.querySelector("#invalCase").innerHTML = "";
+    amountFrets = frets.value;
+    btnCalc.removeAttribute("disabled");
+  }
 }
 // button calculate
-const calc = document.querySelector('#calc');
-calc.addEventListener('click', calculate);
+const calc = document.querySelector("#calc");
+calc.addEventListener("click", calculate);
 
 function calculate() {
-    calc.disabled = true;
-    if (!length || !amountFrets || isNaN(length) && isNaN(amountFrets)) return;
-    if (length * 1 === 0 || amountFrets * 1 === 0) {
-        if (length * 1 === 0) {
-
-            invalidData('#invalidCase');
-        }
-        if (amountFrets * 1 === 0) {
-
-            invalidData('#invalCase');
-        }
-        return
+  calc.disabled = true;
+  if (!length || !amountFrets || (isNaN(length) && isNaN(amountFrets))) return;
+  if (length * 1 === 0 || amountFrets * 1 === 0) {
+    if (length * 1 === 0) {
+      invalidData("#invalidCase");
     }
-    calcGuitarFrets(length, amountFrets, coeficient, unit);
-    let infoBlock = document.querySelector('.calculation');
-    let content = document.querySelector('.wrap');
+    if (amountFrets * 1 === 0) {
+      invalidData("#invalCase");
+    }
+    return;
+  }
+  calcGuitarFrets(length, amountFrets, coeficient, unit);
+  let infoBlock = document.querySelector(".calculation");
+  let content = document.querySelector(".wrap");
 
-    setTimeout(() => {
-        content.style.display = 'none';
-        calc.disabled = false;
-    }, 500);
+  setTimeout(() => {
+    content.style.display = "none";
+    calc.disabled = false;
+  }, 500);
 
-    infoBlock.classList.add('animate');
+  infoBlock.classList.add("animate");
 }
 
 //select rows of table
 let tableBodyEvent = document.body;
-tableBodyEvent.addEventListener('ready-table', selectLines);
+tableBodyEvent.addEventListener("ready-table", selectLines);
 
 function selectLines(event) {
-    let tabLines = Array.from(event.detail.children);
-    table.addEventListener('click', (e) => {
-        let target = e.target;
-        let parent = e.target.parentNode;
-        let grandParent = e.target.parentNode.parentNode;
-        for (let i = 0; i < tabLines.length; i++) {
-            if (tabLines[i].classList.contains('selected')) {
-                tabLines[i].classList.remove('selected');
-            }
-        }
-        if (target.tagName == 'TR') {
-            target.classList.add('selected');
-            target.firstChild.style.color = '1f1f#1f';
-        }
-        if (target.tagName == 'TD') {
-            parent.classList.add('selected');
-            parent.firstChild.style.color = '#1f1f1f';
-        }
-        if (target.tagName == 'SPAN') {
-            grandParent.classList.add('selected');
-            grandParent.firstChild.style.color = '#1f1f1f';
-        }
+  let tabLines = Array.from(event.detail.children);
+  table.addEventListener("click", (e) => {
+    let target = e.target;
+    let parent = e.target.parentNode;
+    let grandParent = e.target.parentNode.parentNode;
+
+    tabLines.forEach((item) => {
+      if (item.classList.contains("selected")) {
+        item.classList.remove("selected");
+      }
     });
+
+    if (target.tagName == "TR") {
+      target.classList.add("selected");
+      target.firstChild.style.color = "1f1f#1f";
+    }
+    if (target.tagName == "TD") {
+      parent.classList.add("selected");
+      parent.firstChild.style.color = "#1f1f1f";
+    }
+    if (target.tagName == "SPAN") {
+      grandParent.classList.add("selected");
+      grandParent.firstChild.style.color = "#1f1f1f";
+    }
+  });
 }
 //hide calculations
-document.querySelector('.fa-long-arrow-left').addEventListener('click', back);
+document.querySelector(".fa-long-arrow-left").addEventListener("click", back);
 
 function back() {
-    let content = document.querySelector('.wrap');
-    setTimeout(() => { content.style.display = 'block' });
-    let animate = document.querySelector('.calculation');
-    animate.classList.remove('animate');
-    setTimeout(() => {
-        document.querySelector('#table').remove();
-    }, 1000);
+  let content = document.querySelector(".wrap");
+  setTimeout(() => {
+    content.style.display = "block";
+  });
+  let animate = document.querySelector(".calculation");
+  animate.classList.remove("animate");
+  setTimeout(() => {
+    document.querySelector("#table").remove();
+  }, 1000);
 }
 
 //download PDF file with calculates
-let download = document.querySelector('.share');
-download.addEventListener('click', confirmDownloadPDF);
+let download = document.querySelector(".share");
+download.addEventListener("click", confirmDownloadPDF);
 
 function confirmDownloadPDF() {
-    const confirmWindow = document.querySelector('.mod_block');
-    confirmWindow.style.display = 'flex';
+  const confirmWindow = document.querySelector(".mod_block");
+  confirmWindow.style.display = "flex";
 }
 
-const modal = document.querySelector('.mod_block');
+const modal = document.querySelector(".mod_block");
 
-modal.addEventListener('click', handlerModalWindow)
+modal.addEventListener("click", handlerModalWindow);
 function handlerModalWindow(e) {
-    const modalBlock = document.querySelector('.mod_block');
-    const modal = document.querySelector('.modal');
-    const target = e.target;
+  const modalBlock = document.querySelector(".mod_block");
+  const target = e.target;
 
-    if (target.tagName === 'BUTTON' && target.classList.contains('cancel')) {
-        modalBlock.style.display = 'none';
-    }
-    else if (target.tagName === 'DIV' && target.classList.contains('mod_block')) {
-        modalBlock.style.display = 'none';
-    }
-    else if (target.tagName === 'BUTTON' && target.classList.contains('ok')) {
-        generatePDF();
-        modal.classList.add('close_modal');
-        setTimeout(() => {
-            modalBlock.style.display = 'none';
-            modal.classList.remove('close_modal')
-        }, 1000);
-    }
+  if (target.tagName === "BUTTON" && target.classList.contains("cancel")) {
+    modalBlock.style.display = "none";
+  } else if (
+    target.tagName === "DIV" &&
+    target.classList.contains("mod_block")
+  ) {
+    modalBlock.style.display = "none";
+  } else if (target.tagName === "BUTTON" && target.classList.contains("ok")) {
+    generatePDF();
+    modal.classList.add("close_modal");
+    setTimeout(() => {
+      modalBlock.style.display = "none";
+      modal.classList.remove("close_modal");
+    }, 1000);
+  }
 }
-
 
 //choose unit measure - mm , inch, none
 let measureUnit = document.forms.unit;
-measureUnit.addEventListener('change', chooseUnit);
+measureUnit.addEventListener("change", chooseUnit);
